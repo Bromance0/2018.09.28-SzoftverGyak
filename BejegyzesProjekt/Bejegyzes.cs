445dcc0190530f30eaf7070d67ccfb8f9261be87
Petrik Lajos Szg <info@petrik.hu>
2018-09-28 15:26:25 +0200

    Elso, masodik feladat

---
+++ BejegyzesProjekt/Bejegyzes.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BejegyzesProjekt
+{
+    class Bejegyzes
+    {
+        public string szerzo;
+        public string tartalom;
+        public int likeok = 0;
+        public DateTime letrejott = DateTime.Now;
+        public DateTime szerkesztve = DateTime.Now;
+
+        
+        public string Szerzo
+        {
+            get
+            {
+                return this.szerzo;
+            }
+        }
+
+        public string Tartalom
+        {
+            get
+            {
+                return this.tartalom;
+            }
+            set
+            {
+                if (this.tartalom != value)
+                {
+                    this.szerkesztve = DateTime.Now;
+                }
+            }
+        }
+
+        public int Likeok
+        {
+            get
+            {
+                return this.likeok;
+            }
+        }
+
+        public DateTime Letrejott
+        {
+            get
+            {
+                return this.letrejott;
+            }
+        }
+
+        public DateTime Szerkesztve
+        {
+            get
+            {
+                return this.szerkesztve;
+            }
+        }
+        
+        public void Like()
+        {
+            likeok++;
+        }
+
+        public void Kiir()
+        {
+            if (szerkesztve == letrejott)
+            {
+                Console.WriteLine("Szerzo" + szerzo + "Likeok" + likeok + "letrjeott" + letrejott + "\nTartalom" + tartalom);
+            }
+            Console.WriteLine("Szerzo" + szerzo + "Likeok" + likeok + "letrjeott" + letrejott +"\nSzerkesztve" + szerkesztve+"\nTartalom" + tartalom);
+        }
+    }
+}
---
+++ BejegyzesProjekt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BejegyzesProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bejegyzesLista = new List<string> { "Kecskét megenni", "Kecskét megint megenni" };
+
+            Console.WriteLine("Adja meg hány bejegyzést akar: ");
+            int darabszam = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < darabszam; i++)
+            {
+                Console.WriteLine("Adja meg a bejegyzést");
+                string bejegyzes = Convert.ToString(Console.ReadLine());
+                bejegyzesLista.Add(bejegyzes);
+            }
+
+        }
+
+
+    }
+}
